d88897620a9195c03f2fdbe60c308a3233c6c2fd
dogredwing1 <dogredwing1@gmail.com>
2014-08-30 10:13:38 -0500

    make compatable with Verizon

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@
 # Kangaroo Kernel build script thanks to CyanogenMod, anarkia1976
 
 # Kernel Version
-BASE_VER=".KANGAROO"
-VER=".M7.mkIV.v027"
+BASE_VER="-AnthraxOS"
+VER="-v027"
 VER=$BASE_VER$VER
 
 # AK Variables
 export LOCALVERSION=""`echo $VER`
 
 # toolchain
-export CROSS_COMPILE=${HOME}/Toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=${HOME}/toolchains/arm-cortex_a15/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
 
 # make .config
 make m7_defconfig
